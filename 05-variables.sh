e1cbcadfc2b36dec1e9b719eae76806ecb9eb7c6
Mahadevan045 <marellamahadevan045@gmail.com>
2024-11-26 21:18:58 +0530

    variables

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,7 +1,15 @@
 #!bin/bash
 
+
+#-s means not show content into the terminal
 echo "Please Enter your username::"
 
-read USERNAME
+read -s USERNAME #takes input in to USERNAME variable
 
 echo "Username is entered is :: $USERNAME"
+
+echo "Please Enter your password::"
+
+read -s PASSWORD #takes input in to PASSWORD variable
+
+echo "Username is entered is :: $PASSWORD"
