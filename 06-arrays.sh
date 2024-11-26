f0401fb2f750c002b1ec2cc7b491d0dfeb69f32a
Mahadevan045 <marellamahadevan045@gmail.com>
2024-11-26 21:31:37 +0530

    variables

--- 06-arrays.sh
+++ 06-arrays.sh
@@ -7,4 +7,4 @@ FRUITS=("APPLE" "KIWI" "ORANGE") #way to define array
 echo "First fruit is ${FRUITS[0]}"
 echo "Second fruit is ${FRUITS[1]}"
 echo "Third fruit is ${FRUITS[2]}"
-echo "All fruits is ${FRUITS[@]}
+echo "All fruits is ${FRUITS[@]}"
