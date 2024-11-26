b88c9b954063a8fe5208d79e97a80608fe090c62
Mahadevan045 <marellamahadevan045@gmail.com>
2024-11-26 21:28:44 +0530

    variables

---
+++ 06-arrays.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+#index starts from 0 and size is 3
+FRUITS=("APPLE" "KIWI" "ORANGE") #way to define array
+
+echo "First fruit is ${FRUITS[0]}"
+echo "Second fruit is ${FRUITS[1]}"
+echo "Third fruit is ${FRUITS[2]}"
