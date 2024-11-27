2ac862dcf21a0a3858348777736c75c165a04ad8
Mahadevan045 <marellamahadevan045@gmail.com>
2024-11-27 08:13:05 +0530

    functions

--- 11-installations.sh
+++ 11-installations.sh
@@ -9,12 +9,12 @@ then
     echo "Please run this script with root previliges"
 fi
 
-dnf list installed gittt
+dnf list installed git
 
 if [ $? -ne 0 ]
 then 
     echo "Git is not installed , going to install it"
-    dnf install gitttt -y
+    dnf install git -y
     if [ $? -ne 0 ]
     then
         echo "Git installation is not success,check it..,"
