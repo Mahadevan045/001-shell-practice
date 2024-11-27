9eee385a1b7250b79dbd434ce03d2b5b8ecc5d23
Mahadevan045 <marellamahadevan045@gmail.com>
2024-11-27 06:45:59 +0530

    functions

--- 07-command-output.sh
+++ 07-command-output.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#Write the command date is command and store the value inside the DATE variable inside the shell script and get the ouput this is the program
 DATE=$(date +%F)
 
 echo "Today's date is : $DATE"
---
+++ 08-data-types.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+NUMBER1=$1
+NUMBER2=$2
+
+#one bracket if for addition and another bracket is for take the value and store into SUM variable
+
+SUM=$((NUMBER1+NUMBER2))
+
+echo "Sum of $NUMBER1 and $NUMBER2 is :: $SUM
