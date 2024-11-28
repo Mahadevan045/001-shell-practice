19777d888181ed5991e1cd049fe445fd5bfcf488
Mahadevan045 <marellamahadevan045@gmail.com>
2024-11-28 21:08:09 +0530

    expense-shell

--- 17-set.sh
+++ 17-set.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-set -e # setting the automatic exit ,if we get error
+set -ex # setting the automatic exit ,if we get error
 echo "Hello World Success"
 echo000 "Hello World failure"
 echo "Hello World After failure"
