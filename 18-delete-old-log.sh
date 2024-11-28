891059755e423797cbc4f47bfd12f23681638564
Mahadevan045 <marellamahadevan045@gmail.com>
2024-11-28 21:46:31 +0530

    expense-shell

--- 18-delete-old-log.sh
+++ 18-delete-old-log.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
 SOURCE_DIR=/home/ec2-user/logs
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
 
 if [ -d $SOURCE_DIR  ]
 then
-    echo "$SOURCE_DIR  Exists"
+    echo -e "$SOURCE_DIR  $G Exists $N"
 else
-    echo "$SOURCE_DIR doesn't exists"
+    echo -e "$SOURCE_DIR $R doesn't exists $N"
     exit 1
 fi
+
+FILES=$(find $SOURCE_DIR -name "*.log" -mtime +14)
+echo "Files are :$FILES"
