3464d39611f0b7aa02db19b57dc5d065f27a9978
Mahadevan045 <marellamahadevan045@gmail.com>
2024-11-28 21:53:40 +0530

    expense-shell

--- 18-delete-old-log.sh
+++ 18-delete-old-log.sh
@@ -16,3 +16,8 @@ fi
 
 FILES=$(find $SOURCE_DIR -name "*.log" -mtime +14)
 echo "Files are :$FILES"
+
+while IFS= read -r line #IFS ,internal field separator,empty it will ignore white spaces,-r is for not to ignore special character like /
+do 
+    echo "Deleting line :$line"
+done <<< $FILES
