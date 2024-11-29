03336ed92193c83efe27419a81d7e9da73885245
Mahadevan045 <marellamahadevan045@gmail.com>
2024-11-29 21:39:53 +0530

    shell-practice

--- 20-disk-usage.sh
+++ 20-disk-usage.sh
@@ -5,5 +5,13 @@ DISK_THRESHOLD==5 #real projects ,it is usually 75
 
 while IFS= read -r file #IFS ,internal field separator,empty it will ignore white spaces,-r is for not to ignore special character like /
 do 
-    echo $line
+    USAGE=$(echo $line | df -hT | grep xfs | awk -F " " '{print $6F}' | cut -d "%" -f1
+)
+    PARITION=$(echo $line df -hT | grep xfs | awk -F " " '{print $NF}'
+)
+
+    if [ $USAGE -ge $DISK_THRESHOLD ]
+    then   
+        echo "$PARITION is more than $DISK_THRESHOLD, current value  :$USAGE,Please check"
+    fi
 done <<< $DISK_USAGE
